13f3b79e27717f7a17140e67e32d85d2ed1cc0e7
Rene <renebults1996@gmail.com>
2022-10-13 22:38:36 +0200

    added donate button to view

--- HallOfFame/View/ContentView.swift
+++ HallOfFame/View/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
+
+
+// create custom button configuration
+struct ScaleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(.blue)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
+
+struct DonatorListView: View {
+    var body: some View {
+        Text("Example text")
+    
+    }
+}
+
+struct ButtonView: View {
+    var body: some View {
+        VStack {
+            Button("Donate") {
+                
+            }.buttonStyle(ScaleButtonStyle())
+        }
+    
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ButtonView()
         }
-        .padding()
+        
     }
 }
 
